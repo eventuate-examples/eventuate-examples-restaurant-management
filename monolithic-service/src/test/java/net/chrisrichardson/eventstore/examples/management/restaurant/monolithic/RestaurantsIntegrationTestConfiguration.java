484bbe355549a831362eb3bc5e72d4364cd05349
dartpopikyardo <stanislav.semichevskiy@digitalarrowtech.com>
2016-08-29 01:33:33 +0300

    - updated eventuate-client-java version - fixed the tests

--- monolithic-service/src/test/java/net/chrisrichardson/eventstore/examples/management/restaurant/monolithic/RestaurantsIntegrationTestConfiguration.java
+++ monolithic-service/src/test/java/net/chrisrichardson/eventstore/examples/management/restaurant/monolithic/RestaurantsIntegrationTestConfiguration.java
@@ -1,5 +1,10 @@
 package net.chrisrichardson.eventstore.examples.management.restaurant.monolithic;
 
+import io.eventuate.javaclient.spring.jdbc.EventuateJdbcEventStoreConfiguration;
+import net.chrisrichardson.eventstore.examples.management.restaurant.commandside.RestaurantsCommandsideConfiguration;
+import net.chrisrichardson.eventstore.examples.management.restaurant.commonweb.WebConfiguration;
+import net.chrisrichardson.eventstore.examples.management.restaurant.queryside.RestaurantsQuerysideConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,24 +15,24 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Configuration
-@Import(RestaurantsConfiguration.class)
+@Import({RestaurantsQuerysideConfiguration.class, RestaurantsCommandsideConfiguration.class, EventuateJdbcEventStoreConfiguration.class, WebConfiguration.class})
+@EnableAutoConfiguration
 public class RestaurantsIntegrationTestConfiguration {
-    @Bean
-    public RestTemplate restTemplate(HttpMessageConverters converters) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
-            @Override
-            protected boolean hasError(HttpStatus statusCode) {
-                return statusCode != HttpStatus.NOT_FOUND && super.hasError(statusCode);
-            }
-        });
-        List<? extends HttpMessageConverter<?>> httpMessageConverters = Collections.singletonList(new MappingJackson2HttpMessageConverter());
-        restTemplate.setMessageConverters((List<HttpMessageConverter<?>>) httpMessageConverters);
-        return restTemplate;
-    }
+  @Bean
+  public RestTemplate restTemplate(HttpMessageConverters converters) {
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+      @Override
+      protected boolean hasError(HttpStatus statusCode) {
+        return statusCode != HttpStatus.NOT_FOUND && super.hasError(statusCode);
+      }
+    });
+    List<? extends HttpMessageConverter<?>> httpMessageConverters = Collections.singletonList(new MappingJackson2HttpMessageConverter());
+    restTemplate.setMessageConverters((List<HttpMessageConverter<?>>) httpMessageConverters);
+    return restTemplate;
+  }
 }
