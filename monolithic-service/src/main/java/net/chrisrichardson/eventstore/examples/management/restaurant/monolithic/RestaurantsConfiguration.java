e26b97bd094e9f831bcbe96599c44bbf8275488b
dartpopikyardo <stanislav.semichevskiy@digitalarrowtech.com>
2016-09-26 21:45:18 +0300

    fix issue#2

--- monolithic-service/src/main/java/net/chrisrichardson/eventstore/examples/management/restaurant/monolithic/RestaurantsConfiguration.java
+++ monolithic-service/src/main/java/net/chrisrichardson/eventstore/examples/management/restaurant/monolithic/RestaurantsConfiguration.java
@@ -2,6 +2,7 @@ package net.chrisrichardson.eventstore.examples.management.restaurant.monolithic
 
 import io.eventuate.javaclient.spring.httpstomp.EventuateHttpStompClientConfiguration;
 import net.chrisrichardson.eventstore.examples.management.restaurant.commandside.RestaurantsCommandsideConfiguration;
+import net.chrisrichardson.eventstore.examples.management.restaurant.commonswagger.CommonSwaggerConfiguration;
 import net.chrisrichardson.eventstore.examples.management.restaurant.commonweb.WebConfiguration;
 import net.chrisrichardson.eventstore.examples.management.restaurant.queryside.RestaurantsQuerysideConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({RestaurantsQuerysideConfiguration.class, RestaurantsCommandsideConfiguration.class, EventuateHttpStompClientConfiguration.class, WebConfiguration.class})
+@Import({RestaurantsQuerysideConfiguration.class, RestaurantsCommandsideConfiguration.class, EventuateHttpStompClientConfiguration.class, WebConfiguration.class, CommonSwaggerConfiguration.class})
 @EnableAutoConfiguration
 @ComponentScan
 public class RestaurantsConfiguration {
--- restaurants-command-side-service/src/main/java/net/chrisrichardson/eventstore/examples/management/restaurant/commandside/RestaurantsServiceCommandsideConfiguration.java
+++ restaurants-command-side-service/src/main/java/net/chrisrichardson/eventstore/examples/management/restaurant/commandside/RestaurantsServiceCommandsideConfiguration.java
@@ -1,6 +1,7 @@
 package net.chrisrichardson.eventstore.examples.management.restaurant.commandside;
 
 import io.eventuate.javaclient.spring.httpstomp.EventuateHttpStompClientConfiguration;
+import net.chrisrichardson.eventstore.examples.management.restaurant.commonswagger.CommonSwaggerConfiguration;
 import net.chrisrichardson.eventstore.examples.management.restaurant.commonweb.WebConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({RestaurantsCommandsideConfiguration.class, EventuateHttpStompClientConfiguration.class, WebConfiguration.class})
+@Import({RestaurantsCommandsideConfiguration.class, EventuateHttpStompClientConfiguration.class, WebConfiguration.class, CommonSwaggerConfiguration.class})
 @EnableAutoConfiguration
 @ComponentScan
 public class RestaurantsServiceCommandsideConfiguration {
