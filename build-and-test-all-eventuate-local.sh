85b3421f69f13a63737011fb6c7141a86960bbae
Chris Richardson <chris@chrisrichardson.net>
2017-01-17 12:20:31 -0800

    Eventuate version upgrades, Misc changes to build-and-test scripts

--- build-and-test-all-eventuate-local.sh
+++ build-and-test-all-eventuate-local.sh
@@ -2,4 +2,5 @@
 
 export EXTRA_INFRASTRUCTURE_SERVICES=cdcservice
 export EVENTUATE_LOCAL=yes
+. ./set-env.sh
 ./_build-and-test-all.sh -f docker-compose-eventuate-local.yml $* -P eventuateDriver=local
