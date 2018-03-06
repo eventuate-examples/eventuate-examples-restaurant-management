3fde3a49153e47ae96e1843ad83fdbf377c68582
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-03-06 14:22:11 +0300

    Supporting Spring Boot 2

--- common-web/src/main/java/net/chrisrichardson/eventstore/examples/management/restaurant/commonweb/WebConfiguration.java
+++ common-web/src/main/java/net/chrisrichardson/eventstore/examples/management/restaurant/commonweb/WebConfiguration.java
@@ -1,19 +1,9 @@
 package net.chrisrichardson.eventstore.examples.management.restaurant.commonweb;
 
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextListener;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan
-public class WebConfiguration extends WebMvcConfigurerAdapter {
-
-    @Bean
-    public ServletListenerRegistrationBean<RequestContextListener> httpRequestContextListener() {
-        return new ServletListenerRegistrationBean<RequestContextListener>(new RequestContextListener());
-    }
-
+public class WebConfiguration {
 }
